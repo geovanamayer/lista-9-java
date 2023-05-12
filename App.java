729aa0c77a972bba352dec95d056698374eea3c8
Geovana Nogueira Mayer <99758327+geovanamayer@users.noreply.github.com>
2023-05-12 18:26:08 -0300

    Update App.java

--- App.java
+++ App.java
@@ -1,5 +1,28 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Exercicio1 {
+
+    public static void main(String[] args) {
+        verificar("Hoje tem aula de POOI");
+        verificar("verdade");
+        verificar("aeiou");
     }
+    
+    
+    public static void verificar(String frase) {
+        int tam = frase.length();
+        int qtde = 0;
+        
+        System.out.println("Total de caracteres da string \"" + frase + "\": " + tam);
+        
+        for (int i = 0; i < tam; i++) {
+            char c = Character.toLowerCase(frase.charAt(i));
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                qtde++;
+            }
+        }
+        
+        System.out.println("Quantidade de vogais: " + qtde);
+
+    }
+
+
 }
