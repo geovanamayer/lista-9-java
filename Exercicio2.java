27aa28709e41c683eeec17b5efebfb4efbfbe1d7
Geovana Nogueira Mayer <99758327+geovanamayer@users.noreply.github.com>
2023-05-12 18:29:41 -0300

    Add files via upload

---
+++ Exercicio2.java
@@ -0,0 +1,24 @@
+public class Exercicio2 {
+
+    public static void main(String[] args) {
+        formarNovaPalavra("abcdefgh");
+        formarNovaPalavra("openai");
+        formarNovaPalavra("abc");
+    }
+    
+
+    public static void formarNovaPalavra(String palavra) {
+        if (palavra.length() < 8) {
+            System.out.println("A palavra deve ter no mínimo 8 caracteres.");
+            return;
+        }
+        
+        String novaPalavra = palavra.substring(0, 2) + palavra.substring(palavra.length() - 2);
+        
+        System.out.println("Palavra original: " + palavra);
+        System.out.println("Nova palavra formada: " + novaPalavra);
+    
+    }
+
+
+}
